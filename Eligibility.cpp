8a14b265b97126cf72b4769c6822f2307df18e86
Suhas M Angadi <93425260+Suhas0107@users.noreply.github.com>
2021-12-26 23:35:50 +0530

    eligibility-for-voting.cpp

---
+++ Eligibility.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int age;
+ cout<<"Enter your Age"<<endl;
+ cin>>age;
+ 
+ if(age>=18)
+ {
+   cout<<" Hey you are eligible for voting"<<endl;
+ }
+ else
+ {
+  cout<<" Sorry! you are not eligible for voting"<<endl;
+  }
+  return 0;
+  }
+  
+ 
