12b34ff7b1f24512bc5c10ccbe29f7303ddb206b
Suhas M Angadi <93425260+Suhas0107@users.noreply.github.com>
2021-12-27 00:29:56 +0530

    created_array_and_accessed_using_pointers.cpp
    by suhas

---
+++ accessing -array_through_pointers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int array[10], *p;
+  cout<<"Enter your elements"<<endl;
+  
+  for(int=0;i<10;i++)
+  {
+    cin>>array[i];
+  }
+  *p=&array[0];
+  for(int=0;i<10;i++)
+  {
+    cout<<"Entered elements are/n"<<array[i]<<endl;
+  }
+  return 0;
+}
+
+    
+    
